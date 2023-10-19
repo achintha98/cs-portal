0b317e6af7cda7091c8fda80139959644c22d846
achintha98 <kalanaachintha@gmail.com>
2023-10-19 23:16:05 +0530

    Created customer microservice

---
+++ customer/src/main/java/com/achintha/customer/CustomerApplication.java
@@ -0,0 +1,16 @@
+package com.achintha.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Achintha Kalunayaka
+ * @since 10/19/2023
+ */
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
