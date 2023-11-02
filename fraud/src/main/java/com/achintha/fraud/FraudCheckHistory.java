d177539824f3f75eca0a1269f6cff5d52b0d31f4
achintha98 <kalanaachintha@gmail.com>
2023-11-02 22:23:09 +0530

    Created database schema for the fraud microservice

---
+++ fraud/src/main/java/com/achintha/fraud/FraudCheckHistory.java
@@ -0,0 +1,35 @@
+package com.achintha.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Achintha Kalunayaka
+ * @since 11/2/2023
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+    private Integer customerId;
+    private boolean isFraudster;
+    private LocalDateTime createdAt;
+}
