3d486dd7fa6771d29a00827a48ed1d3c7f5b1d26
achintha98 <kalanaachintha@gmail.com>
2023-11-02 23:12:48 +0530

    Added fruad api and services

---
+++ fraud/src/main/java/com/achintha/fraud/FraudCheckController.java
@@ -0,0 +1,27 @@
+package com.achintha.fraud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Achintha Kalunayaka
+ * @since 11/2/2023
+ */
+
+@RestController
+@RequestMapping("api/v1/fruad/check")
+public class FraudCheckController {
+    private final FraudCheckService fraudCheckService;
+
+    public FraudCheckController(FraudCheckService fraudCheckService) {
+        this.fraudCheckService = fraudCheckService;
+    }
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}
