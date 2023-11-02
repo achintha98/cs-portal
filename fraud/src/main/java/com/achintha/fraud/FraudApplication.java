ef29ba7bd3e18fbd014a27062b47086450ec2e80
achintha98 <kalanaachintha@gmail.com>
2023-11-02 21:55:58 +0530

    Implemented fraud microservice

---
+++ fraud/src/main/java/com/achintha/fraud/FraudApplication.java
@@ -0,0 +1,16 @@
+package com.achintha.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Achintha Kalunayaka
+ * @since 11/2/2023
+ */
+
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
